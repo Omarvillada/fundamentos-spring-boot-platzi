b998a87deeda412460be43d7043cfcc6f3893351
Omar <olvillada@gmail.com>
2021-10-13 10:33:31 -0500

    Primer commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentosplatzi.springboot.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	//Este metodo inicializa toda la arquitectuta y la configuracion del proyecto springboot aplicacion
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
