bb1f343d1419eee4fade104927f0b925e73f5681
Omar <olvillada@gmail.com>
2021-10-13 12:52:44 -0500

    Ejemplo de creacion de dependencia propia

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,5 +1,7 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBean;
+import com.fundamentosplatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -10,11 +12,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
+	private MyBean myBean;
+	private MyBeanWithDependency myBeanWithDependency;
+
 
 	//Se inyecta dependencia
 	//Qualifier llamamos la dependencia que queremos segregar
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency){
 		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithDependency = myBeanWithDependency;
 	}
 
 	//Este metodo inicializa toda la arquitectuta y la configuracion del proyecto springcboot aplicacion
@@ -25,5 +32,7 @@ public class FundamentosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
+		myBean.print();
+		myBeanWithDependency.printWithDependency();
 	}
 }
